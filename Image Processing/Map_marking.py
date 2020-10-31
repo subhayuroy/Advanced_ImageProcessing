0034e6f7ea4de8ddf3717dfc458f3ce68bf97d42
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-10-31 20:54:33 +0530

    Marking on the map using rectangles

--- Image Processing/Map_marking.py
+++ Image Processing/Map_marking.py
@@ -2,22 +2,40 @@ import cv2
 import numpy as np
 
 # Step 1. Define callback function
-def draw_circle(event, x, y, flags, param):
+drawing = False
+ix = -1
+iy = -1
+
+def draw_rectangle(event, x, y, flags, params):
+
+    global ix, iy, drawing
+
     if event == cv2.EVENT_LBUTTONDOWN:
-            cv2.circle(img, center = (x, y), radius = 5, 
-                       color = (87, 184, 237), thickness = -1)
-    elif event == cv2.EVENT_RBUTTONDOWN:        
-            cv2.circle(img, center = (x, y), radius = 10,  
-                       color = (87, 184, 237), thickness = 1)
+        drawing = True
+        ix, iy = x, y
         
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.rectangle(img, pt1 = (ix, iy), pt2 = (x, y), 
+                          color = (87, 184, 237), thickness = -1)
+            
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.rectangle(img, pt1 = (ix, iy), pt2 = (x, y), 
+                      color = (87, 184, 237), thickness = -1)
+        
+    
 # Step 2. Call the window
 img = cv2.imread('map.png')
-cv2.namedWindow(winname = 'my_drawing')
-cv2.setMouseCallback('my_drawing', draw_circle)
 
-# Step 3. Execution
-while True:
-    cv2.imshow('my_drawing',img)
+cv2.namedWindow(winname = 'my_drawing')
+cv2.setMouseCallback('my_drawing', draw_rectangle)
+
+
+# Step 3. Execution 
+while True: 
+    cv2.imshow('my_drawing', img)
     if cv2.waitKey(10) & 0xFF == 27:
         break
+
 cv2.destroyAllWindows()
