9781ec426cfa528223a5e728b2a1bb203e04792c
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-10-31 20:38:20 +0530

    Converting image to grayscale

--- Image Processing/intro.py
+++ Image Processing/intro.py
@@ -7,5 +7,8 @@ import matplotlib.pyplot as plt
 img = cv2.imread('burano.jpg')  #Import the image
 #plt.imshow(img)
 
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img_rgb)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #Convert the image into RGB
+#plt.imshow(img_rgb)
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(img_gray, cmap='gray')
