3018a2d6c9f1859d758f8d75734f0ce2ba16e872
Subhayu Roy <55306904+subhayuroy@users.noreply.github.com>
2020-10-31 20:42:19 +0530

    Plot the three channels of the image

--- Image Processing/intro.py
+++ Image Processing/intro.py
@@ -10,5 +10,12 @@ img = cv2.imread('burano.jpg')  #Import the image
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #Convert the image into RGB
 #plt.imshow(img_rgb)
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(img_gray, cmap='gray')
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #Convert the image to Grayscale
+#plt.imshow(img_gray, cmap='gray')
+
+fig, axs = plt.subplots(nrows=1, bnols=3, figsize=(20,20))
+
+for i in range(3):
+  ax = axs[i]
+  ax.imshow(img_rgb[:,:,i], cmap='gray')
+plt.show()
